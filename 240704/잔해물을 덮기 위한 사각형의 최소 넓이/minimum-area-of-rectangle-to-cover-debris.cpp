2d6560817e81aa7f55e902d95d137ac6ed7dc1ff
kcyoow <kcy021012@gmail.com>
2024-07-04 11:38:14 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 13ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240704/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
+++ 240704/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
@@ -12,8 +12,8 @@ int main(){
         }
     }
     for(int t = 0; t < 2; t++){
-        for(int i = xy[t][0]; i < xy[t][2]; i++){
-            for(int j = xy[t][1]; j < xy[t][3]; j++){
+        for(int i = xy[t][0]; i <= xy[t][2]; i++){
+            for(int j = xy[t][1]; j <= xy[t][3]; j++){
                 if(t == 0){
                     arr[i][j] = 1;
                 }
@@ -23,23 +23,29 @@ int main(){
             }
         }
     }
-    int maxh = 0;
-    int maxw = 0;
-    int cnth = 0;
+    int maxx = 0;
+    int maxy = 0;
+    int minx = 1e6;
+    int miny = 1e6;
     for(int i = 0; i < sizeof(arr)/sizeof(*arr); i++){
-        int cntw = 0;
         for(int j = 0; j < sizeof(arr[i])/sizeof(*arr[i]); j++){
             if(arr[i][j] == 1){
-                cntw++;
+                if(j > maxx){
+                    maxx = j;
+                }
+                if(j < minx){
+                    minx = j;
+                }
+                if(i > maxy){
+                    maxy = i;
+                }
+                if(i < miny){
+                    miny = i;
+                }
             }
         }
-        if(cntw > 0){
-            cnth++;
-        }
-        if(cntw > maxw) maxw=cntw;
     }
-    maxh=cnth;
-    cout << maxw*maxh;
+    cout << (maxx-minx)*(maxy-miny);
 
 
 }
