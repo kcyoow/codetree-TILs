60c31139d6f62882faf78be71ac2a42b4ec7ce81
kcyoow <kcy021012@gmail.com>
2024-07-15 07:03:18 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240715/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.cpp
+++ 240715/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.cpp
@@ -16,10 +16,7 @@ int main(){
         if(check[p.second+dir[d][1]][p.first+dir[d][0]] != 0 || (p.first+dir[d][0]) < 0 || (p.first+dir[d][0]) >= m || (p.second+dir[d][1]) < 0 || (p.second+dir[d][1]) >= n){d = (d+1)%4;}
         p.first += dir[d][0];
         p.second += dir[d][1];
-        cout << "cnt = " << cnt << endl;
-        cout << p.second << ' ' << p.first << endl;
         check[p.second][p.first] = cnt;
-        // cout << p.first << ' ' << p.second << endl;
     }
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
