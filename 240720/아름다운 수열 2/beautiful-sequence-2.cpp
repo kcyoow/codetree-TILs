003433289e6372b90a6a2dcd1a321b20ae5b9600
kcyoow <kcy021012@gmail.com>
2024-07-20 18:44:33 +0900

    Title: 아름다운 수열 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240720/아름다운 수열 2/beautiful-sequence-2.cpp
+++ 240720/아름다운 수열 2/beautiful-sequence-2.cpp
@@ -22,12 +22,12 @@ int main(){
     int cnt = 0;
     for(int i = 0; i <= N-M; i++){
         vector<int> arr;
-        for(int j = 0; j < 3; j++){
+        for(int j = 0; j < M; j++){
             arr.push_back(arrN[i+j]);
         }
         sort(arr.begin(), arr.end());
         cnt++;
-        for(int j = 0; j < 3; j++){
+        for(int j = 0; j < M; j++){
             if(arr[j] != arrM[j]){
                 cnt--;
                 break;
