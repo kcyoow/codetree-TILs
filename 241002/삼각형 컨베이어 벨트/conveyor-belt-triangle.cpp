6e802785d63e5a4aeb5f7b656594559395267fe1
kcyoow <kcy021012@gmail.com>
2024-10-02 22:55:38 +0900

    Title: 삼각형 컨베이어 벨트, Time: 14ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 241002/삼각형 컨베이어 벨트/conveyor-belt-triangle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a[405], t, n;
+    cin >> n >> t;
+    t%=(3*n);
+    t = t>0?3*n-t:0;
+    for(int i = 0; i < 3*n; i++){
+        cin >> a[i];
+    }
+    for(int i = 0; i < 3*n; i++){
+        cout << a[t] << ' ';
+        t=(t+1)%(3*n);
+        if(i == n-1){
+            cout << endl;
+        } else if( i == 2*n-1){
+            cout << endl;
+        }
+    }
+
+}
