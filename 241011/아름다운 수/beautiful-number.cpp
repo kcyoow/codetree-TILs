bf8196a1924ce048abafeb3c7927d78cc2863788
kcyoow <kcy021012@gmail.com>
2024-10-11 00:18:55 +0900

    Title: 아름다운 수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241011/아름다운 수/beautiful-number.cpp
+++ 241011/아름다운 수/beautiful-number.cpp
@@ -1,2 +1,2 @@
 #import<iostream>
-int ans,n;void choose(int num){if(num<=0)ans++;for(int i=1;i<=4;i++){if(num-i>=0)choose(num-i);}}main(){std::cin>>n;choose(n);std::cout<<ans;}
+int ans,n;void choose(int num){if(num<=0)ans++;for(int i=1;i<=4;i++){if(num-i>=0)choose(num-i);}}int main(){std::cin>>n;choose(n);std::cout<<ans;}
