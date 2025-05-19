04ad7ffe97d70067d1f914ff5799f40f455d7bbb
kcyoow <kcy021012@gmail.com>
2025-05-20 08:27:33 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250520/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
+++ 250520/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
@@ -6,38 +6,44 @@ using namespace std;
 int n;
 int arr[10];
 int test[4] = {0, 0, 0, 0};
-void eee(int n, int num, int cnt){
+void eee(int n, int tests[4]){
     if(n < 0) return;
     if(arr[n] == 1) {
-        if(num==2) test[0]=(test[0]<cnt?cnt:test[0]);
-        if(num==3) test[1]=(test[1]<cnt?cnt:test[1]);
-        if(num==5) test[2]=(test[2]<cnt?cnt:test[2]);
-        if(num==7) test[3]=(test[3]<cnt?cnt:test[3]);
+        for(int i = 0; i < 4; i++){
+            if(test[i]<tests[i])test[i]=tests[i];
+        }
+        return;
     }
     if(arr[n]%2==0){
         arr[n]/=2;
-        eee(n, 2, cnt+1);
+        tests[0]++;
+        eee(n, tests);
     }
     if(arr[n]%3==0){
         arr[n]/=3;
-        eee(n, 3, cnt+1);
+        tests[1]++;
+        eee(n, tests);
     }
     if(arr[n]%5==0){
         arr[n]/=5;
-        eee(n, 5, cnt+1);
+        tests[2]++;
+        eee(n, tests);
     }
     if(arr[n]%7==0){
         arr[n]/=7;
-        eee(n, 7, cnt+1);
+        tests[3]++;
+        eee(n, tests);
     }
-    eee(n-1, 0, 0);
+    int t[4] = {0,};
+    eee(n-1, t);
 }
 int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    eee(n-1, 0, 0);
+    int t2[4] = {0,};
+    eee(n-1,t2);
     int mul=1;
     // Please write your code here.
     for(int i = 0; i < n; i++){
